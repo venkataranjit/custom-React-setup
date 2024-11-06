da950ae120d403fafd0683f42773ef2c5439cd0f
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-11-06 23:34:47 +0530

    babel installed manually

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style.css";
 
 const heading = React.createElement(
   "h1",
-  { id: "title", className: "main-heading" },
-  "Hello React"
+  {
+    id: "main-heading",
+    className: "title",
+  },
+  "custom react setup"
 );
+const para = React.createElement("p", null, "This is Para");
+
+const container = React.createElement(
+  "div",
+  { id: "container" },
+  heading,
+  para
+);
+
+const container2 = <h2>Hello</h2>;
 
 const app = document.getElementById("app");
 const root = ReactDOM.createRoot(app);
 
-root.render(heading);
+root.render(container2);
--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,22 @@ module.exports = {
     },
     port: 3005,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
